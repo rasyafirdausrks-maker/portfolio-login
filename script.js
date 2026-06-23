d3a18d516c97e6432e53fc7870f4285e2399590d
rasyafirdausrks-maker <rasyafirdausrks@gmail.com>
2026-06-23 20:06:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
 // Login
 function login() {
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
+
+    let user =
+        document.getElementById("username").value;
+
+    let pass =
+        document.getElementById("password").value;
 
     if (user === "admin" && pass === "12345") {
+
         localStorage.setItem("login", "true");
+
         window.location.href = "dashboard.html";
+
     } else {
+
         document.getElementById("msg").innerText =
             "Username atau password salah!";
     }
 }
 
-// Cek login
+// Cek Login
 if (window.location.pathname.includes("dashboard")) {
+
     if (localStorage.getItem("login") !== "true") {
+
         window.location.href = "index.html";
     }
 }
 
-// Upload file (simulasi)
+// Upload File
 function uploadFile() {
-    let file = document.getElementById("fileInput").files[0];
+
+    let file =
+        document.getElementById("fileInput").files[0];
 
     if (!file) return;
 
     let li = document.createElement("li");
+
     li.textContent = file.name;
 
-    document.getElementById("fileList").appendChild(li);
+    document.getElementById("fileList")
+        .appendChild(li);
 }
 
 // Logout
 function logout() {
+
     localStorage.removeItem("login");
+
     window.location.href = "index.html";
 }
 
 // Dark Mode
 function toggleDarkMode() {
+
     document.body.classList.toggle("dark-mode");
 }
 
@@ -48,21 +65,26 @@ setInterval(() => {
     let now = new Date();
 
     let time =
-        now.getHours().toString().padStart(2, "0") + ":" +
-        now.getMinutes().toString().padStart(2, "0") + ":" +
+        now.getHours().toString().padStart(2, "0")
+        + ":" +
+        now.getMinutes().toString().padStart(2, "0")
+        + ":" +
         now.getSeconds().toString().padStart(2, "0");
 
     let clock = document.getElementById("clock");
 
     if (clock) {
+
         clock.innerHTML = time;
     }
 
 }, 1000);
 
+// Catatan
 function addNote() {
 
-    let input = document.getElementById("noteInput");
+    let input =
+        document.getElementById("noteInput");
 
     if (input.value === "") return;
 
